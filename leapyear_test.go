7c52a99f90da437c0fd5a5de1373f4bff7a28b2b
team12 <team12@example.com>
2025-06-25 02:54:04 +0000

    subtask1

--- leapyear_test.go
+++ leapyear_test.go
@@ -4,15 +4,8 @@ import (
     "testing"
 )
 
-func TestLeapYear01(t *testing.T) {
-    expected := leapyear(1)
-    actual := false
-    if expected != actual {
-        t.Errorf("Error")
-    }
-}
 
-func TestLeapYear02(t *testing.T) {
+func TestLeapYear01(t *testing.T) {
     expected := leapyear(2)
     actual := false
     if expected != actual {
